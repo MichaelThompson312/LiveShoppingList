698326354520bba4590430ad3157030892493748
Michael Thomposn <mthompson2@idtdna.com>
2021-02-24 19:09:21 -0600

    CLean up

--- shopping-list/src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    apiKey: "AIzaSyAgBvGKlPEySB6vCWVkyO5OnRiVP3pzgps",
-    authDomain: "react-firebase-basic.firebaseapp.com",
-    databaseURL: "https://react-firebase-basic.firebaseio.com"
-  };
-  
-  export default config;
